8f3187f250ae5b10a5e32d96b7a5c985b9affbdf
Piyush-kathait <113622117+Piyush-kathait@users.noreply.github.com>
2023-10-13 04:33:58 +0530

    Update NewtonB.c

--- NewtonB.c
+++ NewtonB.c
@@ -1,53 +1,48 @@
 #include <stdio.h>
-int fact(int);
-int main()
+#include <math.h>
+#define MAXN 100
+#define ORDER 4
+void main()
 {
-    int n, i, j, ch = 30;
-    float arr[10][11], px = 1, x, y, p, h;
-    printf("\nEnter the no of data:");
+    float ax[MAXN + 1], ay[MAXN], diff[MAXN][ORDER + 1], sum, fun, y0, x0, x, p, h, yp;
+    int n, i, j, k;
+    printf("\nEnter the value of n:\n");
     scanf("%d", &n);
-    printf("\nEnter the data:");
+    printf("\nEnter the values in form x,y:\n");
     for (i = 0; i < n; i++)
     {
-        printf("\nX%d=", i + 1);
-        scanf("%f", &arr[i][0]);
-        printf("\nY%d=", i + 1);
-        scanf("%f", &arr[i][1]);
+        scanf("%f %f", &ax[i], &ay[i]);
     }
-    // forming the difference table
-    for (j = 2; j <= n; j++)
-    {
-        for (i = 0; i < n - 1; i++)
-            arr[i][j] = arr[i + 1][j - 1] - arr[i][j - 1];
-    }
-    // printing table
-    printf("\nThe difference table");
-    printf("\n\tX \tY");
-    for (i = 0; i < n - 1; i++)
-        printf("\t%c^%d", ch, i + 1);
-    for (i = 0; i < n; i++)
-    {
-        printf("\n");
-        for (j = 0; j < n + 1 - i; j++)
-            printf("\t%.4f", arr[i][j]);
-    }
-    printf("\nEnter the value of x for f(x)");
+    printf("\nEnter the value of x for");
+    printf("\nwhich you want the value of y - ");
     scanf("%f", &x);
-    // calculate the value of f(x) for x
-    h = arr[n - 1][0] - arr[n - 2][0];
-    p = (x - arr[n - 1][0]) / h;
-    y = arr[n - 1][1];
-    for (i = 1; i < n; i++)
+    h = ax[1] - ax[0];
+    for (i = 0; i < n - 1; i++)
     {
-        px = px * (p + (i - 1));
-        y = y + (arr[n - 1 - i][i + 1] * px) / fact(i);
+        diff[i][1] = ay[i + 1] - ay[i];
     }
-    printf("\nthe value of f(x) at x=%f is %f", x, y);
-}
-int fact(int n)
-{
-    int f = 1, i;
-    for (i = 1; i <= n; i++)
-        f = f * i;
-    return f;
+    for (j = 2; j <= 4; j++)
+    {
+        for (i = 0; i < n - j; i++)
+        {
+            diff[i][j] = diff[i + 1][j - 1] - diff[i][j - 1];
+        }
+    }
+    i = 0;
+    while (!ax[i] > x)
+    {
+        i++;
+    }
+    x0 = ax[i];
+    sum = 0;
+    y0 = ay[i];
+    fun = 1;
+    p = (x - x0) / h;
+    sum = y0;
+    for (k = 1; k <= 4; k++)
+    {
+        fun = (fun * (p - (k - 1))) / k;
+        sum = sum + fun * diff[i][k];
+    }
+    printf("\nwhen x=%0.4f,y=%0.8f", x, sum);
 }
